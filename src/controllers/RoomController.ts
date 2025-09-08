f5a852c7fcd1e837c4d9d00dc5d1c4551e1f9441
BrunoMaiorano <brunomaiorano@gmail.com>
2025-09-08 11:31:19 -0300

    Rooms update, delete controller and routes

--- src/controllers/RoomController.ts
+++ src/controllers/RoomController.ts
@@ -13,30 +13,92 @@ const getAllRooms = async (req: Request, res: Response) => {
   }
 };
 
-const getRoomById = async (req: Request, res: Response) => { 
-    try{
-        const room = await Room.findById(req.params.id)
-        if(!room) {
-            return res.status(404).json({ message: "Room not found"})
-        }
-        res.status(200).json(room)
-    } catch(error) {
-        res.status(500).json({message: "Error fetching Room", error})
+const getRoomById = async (req: Request, res: Response) => {
+  try {
+    const room = await Room.findById(req.params.id);
+    if (!room) {
+      return res.status(404).json({ message: "Room not found" });
     }
-}
+    res.status(200).json(room);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching Room", error });
+  }
+};
 
 const createRoom = async (req: Request, res: Response) => {
-    try{
-        const {name, users, description, createdAt,createdBy, max_players, isPrivate, password, serverIP, serverPort} = req.body
+  try {
+    let {
+      name,
+      users,
+      description,
+      createdAt,
+      createdBy,
+      max_players,
+      isPrivate,
+      password,
+      serverIP,
+      serverPort,
+    } = req.body;
 
-        const room = new Room({name, users, description, createdAt,createdBy, max_players, isPrivate, password, serverIP, serverPort})
-
-        await room.save()
-
-        res.status(201).json({message: "Room created", room})
-    } catch(error) {
-        res.status(500).json({message: "Error creating user", error})
+    if (isPrivate == false) {
+      password = null;
     }
-}
 
-export default {getAllRooms, getRoomById, createRoom}
+    const room = new Room({
+      name,
+      users,
+      description,
+      createdAt,
+      createdBy,
+      max_players,
+      isPrivate,
+      password,
+      serverIP,
+      serverPort,
+    });
+
+    await room.save();
+
+    res.status(201).json({ message: "Room created", room });
+  } catch (error) {
+    res.status(500).json({ message: "Error creating user", error });
+  }
+};
+
+const updateRoom = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    let { name, users, description, max_players, isPrivate, password } =
+      req.params;
+
+    const room = await Room.findByIdAndUpdate(
+      id,
+      { name, users, description, max_players, isPrivate, password },
+      { new: true }
+    );
+
+    if (!room) {
+      return res.status(404).json({ message: "Room not found" });
+    }
+
+    res.status(200).json({ message: "Error updating room", room });
+  } catch (error) {
+    res.status(500).json({ message: "Error updating Room", error });
+  }
+};
+
+const hardDeleteRoom = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const room = await Room.findByIdAndDelete(id);
+    if (!room) {
+      return res.status(404).json({ message: "Room not found" });
+    }
+    res.status(200).json({ message: "Room hard deleted" });
+  } catch (error) {
+    res.status(500).json({ message: "Error hard deleting room", error });
+  }
+};
+
+
+export default { getAllRooms, getRoomById, createRoom, updateRoom, hardDeleteRoom };
--- src/routes/RoomRoutes.ts
+++ src/routes/RoomRoutes.ts
@@ -5,6 +5,8 @@ import controllers from "../controllers/RoomController";
 const RoomRoutes = express.Router();
 
 RoomRoutes.post("/", controllers.createRoom);
+RoomRoutes.patch("/:id", controllers.updateRoom)
+RoomRoutes.delete("/hard/:id", controllers.hardDeleteRoom)
 RoomRoutes.get("/:id", controllers.getRoomById);
 RoomRoutes.get("/", controllers.getAllRooms);
 
